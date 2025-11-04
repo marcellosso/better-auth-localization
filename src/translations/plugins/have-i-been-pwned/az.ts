9058f1fd2a90248e128f1322b70f071131c304ac
Mehdi Asadli <mehdiasadli@azericard.com>
2025-11-04 16:28:37 +0400

    translate: plugin - have-i-been-pwned

---
+++ src/translations/plugins/have-i-been-pwned/az.ts
@@ -0,0 +1,15 @@
+import type { HaveIBeenPwnedErrorCodesType } from '../../../types';
+
+export const AZ = {
+  // Have I Been Pwned ilə bağlı səhvlər
+  PASSWORD_COMPROMISED: 'Daxil etdiyiniz şifrə kompromet olunub. Zəhmət olmasa fərqli bir şifrə seçin.',
+} satisfies HaveIBeenPwnedErrorCodesType;
+export const AZ_AZ = {
+  // Have I Been Pwned ilə bağlı səhvlər
+  PASSWORD_COMPROMISED: 'Daxil etdiyiniz şifrə kompromet olunub. Zəhmət olmasa fərqli bir şifrə seçin.',
+} satisfies HaveIBeenPwnedErrorCodesType;
+
+export const LOCALES = {
+  az: AZ,
+  'az-AZ': AZ_AZ,
+} as const;
